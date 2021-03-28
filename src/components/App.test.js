f402d33d249e18f65770db3f0d289642da6f2974
Your Name <you@example.com>
2021-03-28 11:15:03 +0100

    added unit tests App.js

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
+
+    const app = shallow(<App />);
+
 
 it('Renders the app correctly' , () =>{
     expect(app).toMatchSnapshot();
+});
+
+it('Initializes the state with an empty list of gifts ', () =>{
+   expect(app.state().gifts).toEqual([]); 
+});
+
+it('It adds a new gift to the `state` when clicking on the `add gift` button', () => { 
+    app.find('.btn-add').simulate('click');
+    expect(app.state().gifts).toEqual([{
+        id:1
+    }]);
+});
+it('Adds a new gift to the rendered list when clicking the `add gift` button' , () =>{
+    app.find('.btn-add').simulate('click');
+    expect(app.find('.gift-list').children().length).toEqual(2);
 });
